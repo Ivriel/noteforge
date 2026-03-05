efc69c0516db58b8e956d218799311ce0a1ba8d6
Ivriel <gunawanivriel@gmail.com>
2026-03-05 11:15:47 +0700

    Notebooks - server actions

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, index, jsonb } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -92,4 +92,42 @@ export const accountRelations = relations(account, ({ one }) => ({
   }),
 }));
 
-export const schema = {user, session, account, verification};
+export const notebooks = pgTable("notebooks", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().$onUpdate(() => /* @__PURE__ */ new Date()).notNull(),
+})
+
+export const notebookRelations = relations(notebooks, ({ many, one }) => ({
+  notes: many(notes),
+  user: one(user, {
+    fields: [notebooks.userId],
+    references: [user.id],
+  }),
+}))
+
+export type Notebook = typeof notebooks.$inferSelect; // buat bikin tipe data buat table notebooks
+
+export const notes = pgTable("notes", {
+  id: text("id").primaryKey(),
+  title: text("title").notNull(),
+  content: jsonb("content").notNull(),
+  notebookId: text("notebook_id").notNull().references(() => notebooks.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().$onUpdate(() => /* @__PURE__ */ new Date()).notNull(),
+})
+
+export const noteRelations = relations(notes, ({ one }) => ({
+  notebook: one(notebooks, {
+    fields: [notes.notebookId],
+    references: [notebooks.id],
+  }),
+}))
+
+export type Note = typeof notes.$inferSelect;
+
+export type InsertNotebook = typeof notebooks.$inferInsert;
+
+export const schema = { user, session, account, verification, notebooks, notes };
